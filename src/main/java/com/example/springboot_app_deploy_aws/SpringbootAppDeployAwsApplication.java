ad7ca18a5262776f1df068678206cf3ce6a66f01
deepankar dey <deepankardey12@gmail.com>
2024-09-08 18:47:15 +0530

    first commit

---
+++ src/main/java/com/example/springboot_app_deploy_aws/SpringbootAppDeployAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_app_deploy_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppDeployAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppDeployAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_app_deploy_aws/SpringbootAppDeployAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_app_deploy_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppDeployAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
